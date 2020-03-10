6b7d8964e779fbffcab1ec8644f04830bc62dde4
Aduda-Boaz <60380365+Aduda-Boaz@users.noreply.github.com>
2020-03-10 17:28:39 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,82 @@
 class TodoApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.handlePick = this.handlePick.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.handleDeleteOption = this.handleDeleteOption.bind(this);
+        this.state = {
+            options: []
+        }
+    }
+    handleDeleteOptions() {
+        this.setState(() => {
+            return {
+                options: []
+            }
+        })
+    }
+    handleDeleteOption(optionToRemove) {
+        this.setState((prevState) => {
+            return {
+                options: prevState.options.filter((option) => optionToRemove !== option)
+            }
+        })
+    }
+    handlePick() {
+        const random = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[random];
+        alert(option);
+    }
+    handleAddOption(option) {
+        if (!option) {
+            return 'Enter valid value to add item';
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exist';
+        }
+        this.setState((prevState) => {
+            return {
+                options: prevState.options.concat(option)
+            }
+        })
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+        }
+    }
+    componentDidMount() {
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+            if (options) {
+                this.setState(() => {
+                    return { options }
+                })
+            }
+        } catch (e) {
+            console.log(e);
+        }       
+    }
     render() {
         const title = 'TodoApp';
         const subtitle = 'Organized schedule';
-        const options = ['Thing one', 'Thing two', 'Thing three']
         return (
             <div>
                 <Header title={title} subtitle={subtitle} />
-                <Action />
-                <Options options={options} />
-                <AddOption />
+                <Action 
+                    handlePick={this.handlePick}
+                    hasOptions={this.state.options.length > 0}
+                />
+                <Options 
+                    options={this.state.options}
+                    handleDeleteOptions={this.handleDeleteOptions} 
+                    handleDeleteOption={this.handleDeleteOption}
+                />
+                <AddOption 
+                    handleAddOption={this.handleAddOption}
+                />
             </div>
         )
     }
@@ -22,37 +90,34 @@ const Header = (props) => (
 )
 
 class Action extends React.Component {
-    handlePick() {
-        alert('Seattle is awesome')
-    }
     render() {
         return (
             <div>
-                <button onClick={this.handlePick}>What should I do?</button>
+                <button 
+                    onClick={this.props.handlePick}
+                    disabled={!this.props.hasOptions}>
+                        What should I do?
+                </button>
             </div>
         )
     }
 }
 
-// handleRemoveAll
-// remove data options
-
 class Options extends React.Component {
     constructor(props) {
         super(props);
-        this.handleRemoveAll = this.handleRemoveAll.bind(this);
-    }
-    handleRemoveAll() {
-        console.log('Remove data options');
-        console.log(this.props.options);
     }
     render() {
         return (
             <div>
-                <button onClick={this.handleRemoveAll}>Remove All</button>
+                <button onClick={this.props.handleDeleteOptions}>Remove All</button>
             {this.props.options.map((option) => {
                 return (
-                    <Option key={option} optionText={option} />
+                    <Option 
+                        key={option} 
+                        optionText={option} 
+                        handleDeleteOption={this.props.handleDeleteOption}
+                    />
                 );
             })}
         </div>
@@ -61,20 +126,34 @@ class Options extends React.Component {
 }
 
 const Option = (props)=> (
-    <div>{props.optionText}</div>
+    <div>
+        {props.optionText}
+        <button onClick={() => {props.handleDeleteOption(props.optionText)}}>remove</button>
+    </div>
 )
 
 class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            error: undefined
+        }
+    }
     handleAddOption(e) {
         e.preventDefault();
         const option = e.target.elements.option.value.trim();
-        if (option) {
-            alert(option);
-        }
+        const error = this.props.handleAddOption(option);
+        this.setState(() => {
+            return { error }
+        })
+
+        e.target.elements.option.value = ''
     }
     render() {
         return (
             <div>
+                {this.state.error && <p>{this.state.error}</p>}
                 <form onSubmit={this.handleAddOption}>
                     <input type="text" name="option" />
                     <button>Add Option</button>
